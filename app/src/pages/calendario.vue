599cc1503e3fdaa61b0117a6cf795450b1ea94b5
Mirco <amirco@gmail.com>
2018-08-08 12:17:08 +0200

    aggiunto pagine demo all'app

--- app/src/pages/calendario.vue
+++ app/src/pages/calendario.vue
@@ -1,7 +1,7 @@
 <template>
   <f7-page>
-    <f7-navbar title="Form" back-link="Back"></f7-navbar>
-    <f7-block-title>Calendario</f7-block-title>
+    <f7-navbar title="Calendario" back-link="Back"></f7-navbar>
+    <f7-block-title>Prossime Prenotazioni:</f7-block-title>
 
   </f7-page>
 </template>
